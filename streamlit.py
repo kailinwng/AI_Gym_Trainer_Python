1d220aafb9586201d3d0960baf496b7103bea2f4
Kai Lin Wong <kailinwng31@gmail.com>
2022-03-12 15:06:21 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,12 +2,13 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer #https://github.com/whitphx/streamlit-webrtc
 
 st.title ("A.I. Gym Trainer 💪")
-st.header("this is the markdown")
 st.markdown("Hello there, thank you for using our AI gym trainer program. \nJust like a personal gym trainer, we aim to help you keep track of the number of exercise repetition you performed, along with a pose correction feedback mechanism to correct your pose if needed. \nAt the end of your exercise, a chart will be displayed to show you your performance throughout the exercise. 📈\nExercises supported: Squats, Arm Curl, Situp")
+st.caption("Note. This is a Monash University final year project completed by Kai Lin Wong, Eu Yang Chai and Kee Hong Tan;")
+st.caption(under the supervision of Dr Raphael Phan; completed in year 2021.")
+           
 st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+webrtc_streamer(key="example")
 
 st.checkbox('yes')
 st.button('Click')
