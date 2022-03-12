ca74c63a87dc7c602ae0a7561f014b0c25eb8587
Kai Lin Wong <kailinwng31@gmail.com>
2022-03-12 14:56:54 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title ("A.I. Gym Trainer 💪")
 st.header("this is the markdown")
-st.markdown("Hello there, thank you for using our AI gym trainer program. \nJust like a personal gym trainer, we aim to help you keep track of the number of exercise repetition you performed, along with a pose correction feedback mechanism to correct your pose if needed. \nAt the end of your exercise, a chart will be displayed to show you your performance throughout the exercise. 📈\nExercises supported: Squats, Arm Curl, Situp"
+st.markdown("Hello there, thank you for using our AI gym trainer program. \nJust like a personal gym trainer, we aim to help you keep track of the number of exercise repetition you performed, along with a pose correction feedback mechanism to correct your pose if needed. \nAt the end of your exercise, a chart will be displayed to show you your performance throughout the exercise. 📈\nExercises supported: Squats, Arm Curl, Situp")
 st.subheader("this is the subheader")
 st.caption("this is the caption")
 st.code("x=2021")
