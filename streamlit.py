4c22a3e778f34487fdfd6b73722e86919ab334f4
Kai Lin Wong <kailinwng31@gmail.com>
2022-03-12 17:08:00 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,19 +4,26 @@ from streamlit_webrtc import webrtc_streamer #https://github.com/whitphx/streaml
 
 #https://github.com/whitphx/streamlit-webrtc-example/blob/main/app.py
 
+st.balloons()
+
 st.title ("A.I. Gym Trainer 🏋️‍♂️")
 st.markdown("Hello there, thank you for using our AI gym trainer program.")
 st.markdown("\nJust like a personal gym trainer, this program helps you keep track of the number of exercise repetition you performed, along with a pose correction feedback mechanism to correct your pose if needed. ✔️")
 st.markdown("\nAt the end of your exercise, a chart will be displayed to show you your performance throughout the exercise. 📈")
 st.markdown("\nExercises supported: Squats, Arm Curl, Situp")
 
-st.caption("Note. This is a Monash University final year project completed by Kai Lin Wong, Eu Yang Chai and Kee Hong Tan; under the supervision of Dr Raphael Phan; completed in year 2021.")
+st.caption("Note. This program is a Monash University final year project completed by Kai Lin Wong, Eu Yang Chai and Kee Hong Tan; under the supervision of Dr Raphael Phan; completed in year 2021.")
+st.caption("This Streamlit app
 st.caption("Github link: https://github.com/kailinwng/AI_Gym_Trainer_Python")   
 
 st.header("Instructions:")
 instruction = "<p style='color:Red;'>Make sure that you have enabled the camera on your computer before proceeding! 💻</p>"
 st.markdown(instruction, unsafe_allow_html=True)
 
+exc = st.radio('Pick your exercise:',['Squats','Armcurl','Situp'])
+
 
 webrtc_streamer(key="frame")
 
+
+st.success("You did it!")
