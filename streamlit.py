bc406fab56aa50699ed602cc8f1d7afe04417136
Kai Lin Wong <kailinwng31@gmail.com>
2022-03-16 18:30:39 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,14 +13,14 @@ st.caption("This Streamlit app is built by Kai Lin Wong: https://www.linkedin.co
 st.caption("Github link: https://github.com/kailinwng/AI_Gym_Trainer_Python")  
 
 st.markdown("\nExercises supported: ")
-gifs = ['https://images.squarespace-cdn.com/content/v1/54f9e84de4b0d13f30bba4cb/1530742878727-5TT9N6GWHG8SQUPVO1WQ/ke17ZwdGBToddI8pDm48kMh3mVmBaCAeGwqCLG3iONRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIBW4H-Ca6AoigG7Ta8YXcF_lHpbhrmZNZWbxxrH_bJLk/bodyweight+squat.gif',"https://www.spotebi.com/wp-content/uploads/2014/10/biceps-curl-exercise-illustration.gif","https://media1.tenor.com/images/c6e16a9b9dc7d97b0d4ad78b50e7b424/tenor.gif?itemid=3579635"]
-capt = ['Squats','Bicep curl','Situp']
+gifs = ['https://images.squarespace-cdn.com/content/v1/54f9e84de4b0d13f30bba4cb/1530742878727-5TT9N6GWHG8SQUPVO1WQ/ke17ZwdGBToddI8pDm48kMh3mVmBaCAeGwqCLG3iONRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIBW4H-Ca6AoigG7Ta8YXcF_lHpbhrmZNZWbxxrH_bJLk/bodyweight+squat.gif',"https://media1.tenor.com/images/c6e16a9b9dc7d97b0d4ad78b50e7b424/tenor.gif?itemid=3579635","https://www.spotebi.com/wp-content/uploads/2014/10/biceps-curl-exercise-illustration.gif"]
+capt = ['Squats','Situp','Bicep curl']
 st.image(gifs, width= 200, caption = capt)
 st.markdown("\nNote: Bicep curls must be done with both arms performing the same motion together.")
 
 st.header("Let's Begin! 💦")
 
-exc = st.radio('Pick your exercise:', ['Squats','Bicep curl','Situp'])
+exc = st.radio('Pick your exercise:', ['Squats','Situp','Bicep curl'])
 
 cam = "<p style='color:Red;'>Make sure that you have enabled the camera on your computer before proceeding! 💻</p>"
 st.markdown(cam, unsafe_allow_html=True)
